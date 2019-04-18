d3e75cc00f8f712722fe1fcc4750cb197a1defc4
Eric Johnson <31421296+ejohn156@users.noreply.github.com>
2019-04-18 19:43:04 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <weather/>
+  </v-app>
+</template>
+
+<script>
+import weather from './components/weather'
+
+export default {
+  name: 'App',
+  components: {
+    weather,
+ }
+}
+</script>
---
+++ src/components/p5.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <v-container grid-list-x1>
+            <v-card style="background-color:#1b5e20;">
+                <v-card-title primary-title>
+                    <h1 class="headline mb-0" style="color:gold">Weather Simulation</h1>
+                </v-card-title>
+                    <v-layout row>
+                        <v-flex pa-12 style="align-content: center;padding-bottom: 3%">
+                            <vue-p5 v-on="this" style="margin-left: 8%" class="justify-center"></vue-p5>
+                        </v-flex>
+                    </v-layout>
+            </v-card>
+        </v-container>
+        </div>
+</template>
+<script>
+import vueP5 from "vue-p5"
+export default {
+    name: 'sim',
+    data: function(){return{weatherData: []}},
+    methods: {
+        setup(sketch) {
+            sketch.createCanvas(700, 500);
+            sketch.background(0);
+            
+        },
+    },
+    props: {
+        currentWeather: Object,
+        forecastArray: Object
+    },
+    components:{
+        "vue-p5": vueP5
+    }
+
+}
+</script>
